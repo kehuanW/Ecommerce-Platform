1f6bfeddaf5d33efc712fe2ad0bc4af7b1a6220d
Kehuan <kehuanwang123@gmail.com>
2022-12-01 12:26:38 +1030

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import Home from "./pages/Home";
+import Navbar from './components/Navbar';
+import Announcement from './components/Announcement'
+
 const App = () => {
-  return <div>Hello world!</div>;
+  return (
+    <div>
+      <Announcement />
+      <Navbar />
+    </div>
+  )
 };
 
 export default App;
