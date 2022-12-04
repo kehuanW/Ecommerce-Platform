36e1d63a99d7b87e3c6e60e29e788c4f362ddde1
Kehuan <kehuanwang123@gmail.com>
2022-12-04 16:55:35 +1030

    login and register page ui

---
+++ src/pages/Register.jsx
@@ -0,0 +1,80 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background: linear-gradient(
+        rgba(225,225,225,0.5),
+        rgba(225,225,225,0.5)
+    ),
+    url("https://images.unsplash.com/photo-1483985988355-763728e1935b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80")
+    no-repeat center/100%;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+const Wrapper = styled.div`
+    width: 40%;
+    padding: 20px;
+    background-color: white
+`
+
+const Title = styled.h1`
+    font-size: 24px;
+    font-weight: 30px;
+    text-align: center;
+`
+
+const Form = styled.form`
+    display: flex;
+    flex-wrap: wrap;
+`
+
+const Input = styled.input`
+    flex:1;
+    min-width: 40%;
+    margin: 20px 10px 0px 0px;
+    padding: 10px;
+`
+
+const Agreement = styled.span`
+    font-size: 12px;
+    margin: 20px 0px;
+`
+
+const Button = styled.button`
+    width: 40%;
+    border: none; 
+    background-color: teal;
+    padding: 15px 20px;
+    color: white;
+    cursor: pointer;
+    margin: auto; // button
+`
+
+const Register = () => {
+    return (
+        <Container>
+            <Wrapper>
+                <Title>CREATE AN ACCOUNT</Title>
+                <Form>
+                    <Input placeholder="name" />
+                    <Input placeholder="last name" />
+                    <Input placeholder="username" />
+                    <Input placeholder="email" />
+                    <Input placeholder="password" />
+                    <Input placeholder="confirm password" />
+                    <Agreement>
+                        By creating an account, I consent to the processing of my personal data in accordance with the <b>PRIVACY POLICY</b>
+                    </Agreement>
+                    <Button>CREATE</Button>
+                </Form>
+            </Wrapper>
+        </Container>
+    )
+}
+
+export default Register
