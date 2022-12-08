e1562e2a40e41871c6a2b8a5d1db21f510599920
Kehuan <kehuanwang123@gmail.com>
2022-12-08 14:33:25 +1030

    update app route v5

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,28 @@
 import { BrowserRouter as Router, Route, Switch, } from "react-router-dom";
-// import Home from "./pages/Home";
-// import ProductList from './pages/ProductList'
-// import Product from './pages/Product'
-// import Register from './pages/Register'
-// import Login from './pages/Login'
-// import Cart from './pages/Cart'
+import Home from "./pages/Home";
+import ProductList from './pages/ProductList'
+import Product from './pages/Product'
+import Register from './pages/Register'
+import Login from './pages/Login'
+import Cart from './pages/Cart'
 import Pay from './Pay';
 import Success from './Success'
 
 const App = () => {
+  const user = true;
   return (
     <div>
-      {/* <Home /> */}
-      {/* <ProductList /> */}
-      {/* <Product /> */}
-      {/* <Register /> */}
-      {/* <Login /> */}
-      {/* <Cart /> */}
       <Router>
         <Switch>
-          <Route path="/pay"><Pay /></Route>
-          <Route path="/success"><Success /></Route>
+          <Route path="/" exact component={Home}>
+          </Route>
+          <Route path="/products" component={ProductList}></Route>
+          <Route path="/product" component={Product}></Route>
+          <Route path="/cart" component={Cart}></Route>
+          <Route path="/login" component={user ? Home : Login}></Route>
+          <Route path="/register" component={user ? Home : Register}></Route>
+          <Route path="/pay" component={Pay}></Route>
+          <Route path="/success" component={Success}></Route>
         </Switch>
       </Router>
     </div>
