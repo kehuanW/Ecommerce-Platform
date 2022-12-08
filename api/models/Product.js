5c2c93840c537ac49b3d2a055e81ab085a94ba4c
Kehuan <kehuanwang123@gmail.com>
2022-12-08 12:38:54 +1030

    Update Product Schema

--- api/models/Product.js
+++ api/models/Product.js
@@ -7,9 +7,10 @@ const ProductSchema = new Schema(
         desc: { type: String, required: true },
         img: { type: String, required: true },
         categories: { type: Array },
-        size: { type: String },
-        color: { type: String },
+        size: { type: Array },
+        color: { type: Array },
         price: { type: Number, required: true },
+        inStock: { type: Boolean, default: true },
     },
 
     // timestamps: true, Mongoose will add two properties (createdAt and updatedAt) of type Date to your schema
