a4365fce08aad9165f2feb075a26478f1d00f3f3
Kehuan <kehuanwang123@gmail.com>
2023-01-15 15:50:39 +1030

    deploy

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const path = require('path')
 
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/user");
@@ -38,6 +39,27 @@ app.use('/api/orders', orderRoute);
 app.use('/api/carts', cartRoute);
 app.use('/api/checkout', stripeRoute);
 app.use('/api/subscribe', subscriptioRoute);
+
+const _dirname = path.dirname("")
+const buildPath = path.join(_dirname, "../client/build");
+
+app.use(express.static(buildPath))
+console.log(buildPath)
+console.log(_dirname)
+
+
+app.get("/*", function (req, res) {
+
+    res.sendFile(
+        path.join(__dirname, "../client/build/index.html"),
+        function (err) {
+            if (err) {
+                res.status(500).send(err);
+            }
+        }
+    );
+
+})
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("Backend is running!")
